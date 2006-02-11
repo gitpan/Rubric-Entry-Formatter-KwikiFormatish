11fd14dbdd567224eacfb38696e7d5a2b08daf55
Ricardo SIGNES <rjbs@cpan.org>
2006-02-11 23:26:42 +0000

    Import of RJBS/Rubric-Entry-Formatter-KwikiFormatish-0.01 from CPAN.
    gitpan-cpan-distribution: Rubric-Entry-Formatter-KwikiFormatish
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         RJBS/Rubric-Entry-Formatter-KwikiFormatish-0.01.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Rubric/Entry/Formatter/KwikiFormatish.pm
@@ -0,0 +1,63 @@
+package Rubric::Entry::Formatter::KwikiFormatish;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Rubric::Entry::Formatter::KwikiFormatish - format entries with AlmostKwikiText
+
+=head1 VERSION
+
+ $Id: /my/cs/projects/rubric/trunk/lib/Rubric/Entry/Formatter/Nil.pm 18100 2006-01-26T13:59:16.285684Z rjbs  $
+
+=head1 DESCRIPTION
+
+This formatter will use KwikiFormatish to format entries into HTML.
+
+=cut
+
+use strict;
+use warnings;
+
+use Text::KwikiFormatish;
+
+=head1 METHODS
+
+=head2 C< as_html >
+
+=head2 C< as_text >
+
+=cut
+
+sub as_html {
+  my ($class, $arg, $config) = @_;
+  $config ||= {};
+  return Text::KwikiFormatish::format($arg->{text}, %$config);
+}
+
+sub as_text {
+  my ($class, $arg) = @_;
+
+  return $arg->{text};
+}
+
+=head1 TODO
+
+=head1 AUTHOR
+
+Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rubric@rt.cpan.org>, or
+through the web interface at L<http://rt.cpan.org>. I will be notified, and
+then you'll automatically be notified of progress on your bug as I make
+changes.
+
+=head1 COPYRIGHT
+
+Copyright 2005 Ricardo SIGNES.  This program is free software;  you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
